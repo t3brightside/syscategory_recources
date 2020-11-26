193f3ed9429ee80233ad4a52e379da98f84125a8
Tanel <tanelbrightside.ee>
2020-11-26 21:01:15 +0200

    Remove TS template

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-	'syscategory_resources',
-	'Configuration/TypoScript/',
-	'System Category Resources'
-);
